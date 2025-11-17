80dd8757621dabd63d1be4cc3dc8c03c062bec4b
Samuel Henares Choaibi <samuelhenares@paucasesnovescifp.cat>
2025-11-17 11:25:10 +0100

    Clase 'Aeropuerto' definido + cambio de nombre de variable y getter de Estado de la clase 'Avion'

--- src/aeropuerto/Aeropuerto.java
+++ src/aeropuerto/Aeropuerto.java
@@ -1,4 +1,29 @@
 package aeropuerto;
 
+import utils.Estados;
+
+import java.util.List;
+
 public class Aeropuerto {
+    private List<Avion> aviones;
+
+    public Aeropuerto(List<Avion> aviones) {
+        this.aviones = aviones;
+    }
+
+    public List<Avion> getAviones() {
+        return aviones;
+    }
+
+    public void setAviones(List<Avion> aviones) {
+        this.aviones = aviones;
+    }
+
+    public void mostrarEstado() {
+        System.out.println("-------- ESTADO DE LOS AVIONES --------");
+        for (Avion avion : aviones) {
+            System.out.println("[" + avion.getId() + "] -> " + avion.getEstado());
+        }
+        System.out.println("---------------------------------------");
+    }
 }
